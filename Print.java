6739159aeeec6bbac7602727bc52c29b6a0f6f09
Pankaj Pareek <71495471+pankajpareekraj@users.noreply.github.com>
2021-10-23 20:33:57 +0530

    Add files via upload

---
+++ Print.java
@@ -0,0 +1,8 @@
+class disp{
+public static void main(String[] args)
+{
+int a=45;
+char s='j';
+System.out.println("int a :%d \n string s: %s",a,s );
+}
+}
---
+++ codition.java
@@ -0,0 +1,8 @@
+class condition{
+public static void main(String sr[])
+{
+for(system.out.println("hiii");;system.out.println("Pareek");
+{
+}
+}
+}
